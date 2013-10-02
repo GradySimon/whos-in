ceb28a30791984ba4943ae7a037912b8bc38b109
Grady Simon <grady@gradysimon.com>
2013-10-02 19:32:09 -0400

    Fixed up the basic project config

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject whos-in "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :description "Who's In - A simple web app for finding collaborators."
+  :url "https://github.com/GradySimon/whos-in"
+  :license {:name "The MIT License (MIT)"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+  				 [ring/ring-jetty-adapter "1.2.0"]
+  				 [compojure "1.1.5"]])
